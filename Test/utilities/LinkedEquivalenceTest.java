5bf7e91c63367f44e8b57c1134103c6824a35f25
Moultrie Dangerfield <moultriedanger@gmail.com>
2023-09-17 21:56:37 -0400

    Add method for equivalence class

--- Test/utilities/LinkedEquivalenceTest.java
+++ Test/utilities/LinkedEquivalenceTest.java
@@ -2,6 +2,8 @@ package utilities;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.Comparator;
+
 import org.junit.jupiter.api.Test;
 
 class LinkedEquivalenceTest {
@@ -9,6 +11,16 @@ class LinkedEquivalenceTest {
 	@Test
 	void test() {
 		
+		
+		
+		Comparator<Integer> c = new Comparator<Integer>()
+		 {
+		 // All even integers are 'equivalent'
+		// All odd integers are 'equivalent'
+		public int compare(Integer x, Integer y)
+		 { return x % 2 == y % 2 ? 0 : 1; }
+		 };
+		 
 	}
 
 }
--- src/utilities/LinkedEquivalenceClass.java
+++ src/utilities/LinkedEquivalenceClass.java
@@ -2,13 +2,14 @@ package utilities;
 
 import java.util.Comparator;
 
-public class LinkedEquivalenceClass<T> implements Comparator<T>{
+public class LinkedEquivalenceClass<T>{
 	
 	protected T _canonical;
 	protected Comparator<T> _comparator;
 	protected LinkedList<T> _rest;
 	
 	public LinkedEquivalenceClass(Comparator<T> comparator) {
+		_canonical = null;
 		_comparator = comparator;
 		_rest = new LinkedList<T>();
 	}
@@ -17,9 +18,7 @@ public class LinkedEquivalenceClass<T> implements Comparator<T>{
 		return _canonical;
 	}
 	
-	
 	public boolean isEmpty() {
-		
 		return _rest.isEmpty();
 	}
 	
@@ -35,15 +34,19 @@ public class LinkedEquivalenceClass<T> implements Comparator<T>{
 	public int size() {
 		return _rest.size();
 	}
+	
+	public boolean add(T element) {
 		
-	
-	
-	@Override
-	public int compare(T o1, T o2) {
-	
-		return 0;
+		if(this.isEmpty()) {
+			_canonical = element;
+			return true;
+		}
+		else if(_comparator.compare(_canonical, element)==0) {
+			_rest.addToFront(element);
+			return true;
+		}
+		return false;
 	}
-	
 	
 
 }
