3e6b666ea07af6cd27a9a5bb63638432bf432cb3
jfpattes <jfpattes@gmail.com>
2023-09-28 15:39:15 -0400

    Added needed files

---
+++ src/input/exception/ParseException.java
@@ -0,0 +1,29 @@
+package input.exception;
+
+public class ParseException extends RuntimeException {
+
+	public ParseException() {
+		// Auto-generated constructor stub
+	}
+
+	public ParseException(String message) {
+		super(message);
+		// Auto-generated constructor stub
+	}
+
+	public ParseException(Throwable cause) {
+		super(cause);
+		// Auto-generated constructor stub
+	}
+
+	public ParseException(String message, Throwable cause) {
+		super(message, cause);
+		// Auto-generated constructor stub
+	}
+
+	public ParseException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+		super(message, cause, enableSuppression, writableStackTrace);
+		// Auto-generated constructor stub
+	}
+
+}
