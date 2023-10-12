37ccc2208f87378ec466c52e5a96fb58987486b5
nutellajay <rrlivingston45@gmail.com>
2023-10-12 14:36:00 -0400

    geometrybuilderfile created

---
+++ src/input/builder/GeometryBuilder.java
@@ -0,0 +1,19 @@
+package input.builder;
+
+import java.util.List;
+
+import input.components.*;
+
+
+public class GeometryBuilder extends DefaultBuilder {
+	
+	public GeometryBuilder() {
+	
+		
+		
+	}
+	
+	@Override
+	public FigureNode buildFigureNode(String description, PointNode points, SegmentNode segments) {}
+	
+};
