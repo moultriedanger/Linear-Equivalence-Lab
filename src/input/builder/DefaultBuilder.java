c57bbbde51a6c6567a2a471c261751e785757011
nutellajay <rrlivingston45@gmail.com>
2023-10-12 15:21:39 -0400

    reconfigured GeometryBuilder

--- src/input/builder/DefaultBuilder.java
+++ src/input/builder/DefaultBuilder.java
@@ -23,31 +23,26 @@ public class DefaultBuilder
     
     public SegmentNodeDatabase buildSegmentNodeDatabase()
     {
-        return new SegmentNodeDatabase();
+        return null;
     }
     
     public void addSegmentToDatabase(SegmentNodeDatabase segments, PointNode from, PointNode to)
     {
-    	if (segments != null) segments.addUndirectedEdge(from, to);
+    	
     }
     
     public SegmentNode buildSegmentNode(PointNode pt1, PointNode pt2)
     {
-        return new SegmentNode(pt1,pt2);
+        return null;
     }
     
     public PointNodeDatabase buildPointDatabaseNode(List<PointNode> points)
     {
-    	PointNodeDatabase pnd = new PointNodeDatabase();
-    	
-    	for(PointNode p: points) {
-    		pnd.put(p);
-    	}
-    	return pnd;
+    	return null;
     }
     
     public PointNode buildPointNode(String name, double x, double y)
     {
-        return new PointNode(name, x,y);
+        return null;
     }
 }
--- src/input/builder/GeometryBuilder.java
+++ src/input/builder/GeometryBuilder.java
@@ -1,5 +1,7 @@
 package input.builder;
 
+import java.util.HashMap;
+import java.util.LinkedHashSet;
 import java.util.List;
 
 import input.components.*;
@@ -9,11 +11,47 @@ public class GeometryBuilder extends DefaultBuilder {
 	
 	public GeometryBuilder() {
 	
-		
-		
 	}
 	
 	@Override
-	public FigureNode buildFigureNode(String description, PointNode points, SegmentNode segments) {}
+	public FigureNode buildFigureNode(String description, PointNodeDatabase points, SegmentNodeDatabase segments) {
 	
-};
+		
+		return new FigureNode(description, points, segments);
+	}
+	
+	@Override
+	public SegmentNodeDatabase buildSegmentNodeDatabase()
+    {
+        return new SegmentNodeDatabase();
+    }
+	
+    @Override
+    public void addSegmentToDatabase(SegmentNodeDatabase segments, PointNode from, PointNode to)
+    {
+    	if (segments != null) segments.addUndirectedEdge(from, to);
+    }
+    
+    @Override
+    public SegmentNode buildSegmentNode(PointNode pt1, PointNode pt2)
+    {
+        return new SegmentNode(pt1,pt2);
+    }
+    
+    @Override
+    public PointNodeDatabase buildPointDatabaseNode(List<PointNode> points)
+    {
+    	PointNodeDatabase pnd = new PointNodeDatabase();
+    	
+    	for(PointNode p: points) {
+    		pnd.put(p);
+    	}
+    	return pnd;
+    }
+    
+    @Override
+    public PointNode buildPointNode(String name, double x, double y)
+    {
+        return new PointNode(name, x,y);
+    }
+}
